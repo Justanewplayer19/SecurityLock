50008f5a464d5cacf4034514dab6090a2d51e4b1
iloveandmissmygf <89791085+Justanewplayer19@users.noreply.github.com>
2025-09-26 20:29:06 -0500

    Update Settings.tsx

--- plugins/template/src/Settings.tsx
+++ plugins/template/src/Settings.tsx
@@ -1,8 +1,53 @@
+// index.js
+import { storage } from "@vendetta/plugin";
 import { Forms } from "@vendetta/ui/components";
-const { FormText } = Forms;
+import { showToast } from "@vendetta/ui/toasts";
+const { FormSection, FormInput, FormSwitchRow, FormText } = Forms;
 
-export default () => (
-    <FormText>
-        Hello, world!
-    </FormText>
-)
+export default {
+  onLoad: () => {
+    // Initialize default storage if missing
+    if (!storage.lockType) storage.lockType = "passcode";
+    if (!storage.passcode) storage.passcode = "";
+    if (!storage.password) storage.password = "";
+    if (storage.enabled === undefined) storage.enabled = false;
+  },
+
+  onUnload: () => {},
+
+  settings: () => (
+    <>
+      <FormSection title="SecurityLock Settings">
+        <FormSwitchRow
+          label="Enable Lock"
+          subLabel="Toggle Discord lock on startup"
+          value={storage.enabled}
+          onValueChange={(v) => (storage.enabled = v)}
+        />
+
+        <FormText>
+          Choose whether to lock Discord with a passcode or a password.  
+          You’ll be asked for it when launching Discord if lock is enabled.
+        </FormText>
+
+        <FormInput
+          title="Passcode"
+          placeholder="Enter numeric passcode"
+          value={storage.passcode}
+          onChange={(v) => (storage.passcode = v)}
+        />
+
+        <FormInput
+          title="Password"
+          placeholder="Enter password"
+          value={storage.password}
+          onChange={(v) => (storage.password = v)}
+        />
+
+        <FormText>
+          Current lock type: {storage.lockType}
+        </FormText>
+      </FormSection>
+    </>
+  ),
+};
